f520a2fb3a3a264c775ea3922012615fac3422a8
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-04 21:54:04 +1100

    images added for upload button, code reworked with help from teacher, now works correctly

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -29,10 +29,11 @@ function elapse(){
             // console.log(name);
             if(check == match){
                 // console.log(check + " - " + name + " - match");
-                document.getElementById(name).style.border = "1px solid Black";
+                document.getElementById(name).style.boxShadow = "none";
             }else{
                 // console.log(check + " - " + name + " - no match");
-                document.getElementById(name).style.border = "5px solid Red";
+                document.getElementById(name).style.boxShadow = "0px 0px 15px Red";
+
             }
             var row = "block"+loopTime;
             if(nowTime > loopTime){
@@ -72,43 +73,43 @@ function checkEvents(){
     if(!eventList){
         var block9={
             time:"9am",
-            event:"test"
+            event:" "
         }
         var block10={
             time:"10am",
-            event:""
+            event:" "
         }
         var block11={
             time:"11am",
-            event:""
+            event:" "
         }
         var block12={
             time:"12pm",
-            event:"blue"
+            event:" "
         }
         var block13={
             time:"1pm",
-            event:""
+            event:" "
         }
         var block14={
             time:"2pm",
-            event:""
+            event:" "
         }
         var block15={
             time:"3pm",
-            event:""
+            event:" "
         }
         var block16={
             time:"4pm",
-            event:""
+            event:" "
         }
         var block17={
             time:"5pm",
-            event:""
+            event:" "
         }
         var block18={
             time:"6pm",
-            event:""
+            event:" "
         }
         var list = [];
         list.push(block9,block10,block11,block12,block13,block14,block15,block16,block17,block18);
@@ -117,29 +118,38 @@ function checkEvents(){
     }
 }
 
-var button1 = document.getElementById("btn9").addEventListener("click",set9);
-var button2 = document.getElementById("btn10").addEventListener("click",set10);
-var button3 = document.getElementById("btn11").addEventListener("click",set11);
-var button4 = document.getElementById("btn12").addEventListener("click",set12);
-var button1 = document.getElementById("btn13").addEventListener("click",set13);
-var button2 = document.getElementById("btn14").addEventListener("click",set14);
-var button3 = document.getElementById("btn15").addEventListener("click",set15);
-var button4 = document.getElementById("btn16").addEventListener("click",set16);
-var button3 = document.getElementById("btn17").addEventListener("click",set17);
-var button4 = document.getElementById("btn18").addEventListener("click",set18);
-// button1.addEventListener("click",test1);
-// button2.addEventListener("click",test2);
-// button3.addEventListener("click",test3);
-// button4.addEventListener("click",test4);
+document.getElementById("btn9").addEventListener("click",set9);
+document.getElementById("btn10").addEventListener("click",set10);
+document.getElementById("btn11").addEventListener("click",set11);
+document.getElementById("btn12").addEventListener("click",set12);
+document.getElementById("btn13").addEventListener("click",set13);
+document.getElementById("btn14").addEventListener("click",set14);
+document.getElementById("btn15").addEventListener("click",set15);
+document.getElementById("btn16").addEventListener("click",set16);
+document.getElementById("btn17").addEventListener("click",set17);
+document.getElementById("btn18").addEventListener("click",set18);
+
+var imageLoc = "url('../images/upload.gif')";
 
 function set9(){
     var toAdd = document.getElementById("9text").value;
     var current = JSON.parse(localStorage.getItem("eventList"));
     current[0].event = toAdd;
     localStorage.setItem("eventList",JSON.stringify(current));
+    document.getElementById("btn9").style.backgroundImage = "url('../assets/images/upload.gif')";
+    setTimeout(function(){
+        document.getElementById("btn9").style.backgroundImage = "";
+    }, 3500);
 }
 function set10(){
-    button2.innerHTML = "test";
+    var toAdd = document.getElementById("10text").value;
+    var current = JSON.parse(localStorage.getItem("eventList"));
+    current[1].event = toAdd;
+    localStorage.setItem("eventList",JSON.stringify(current));
+    document.getElementById("btn10").style.backgroundImage = "url('../assets/images/upload.gif')";
+    setTimeout(function(){
+        document.getElementById("btn10").style.backgroundImage = "";
+    }, 3500);
 }
 function set11(){
     button3.innerHTML = "test";
