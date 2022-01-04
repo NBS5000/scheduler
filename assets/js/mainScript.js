bfe4c1f7340ea2479e22024a4753fd8273014e33
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-04 17:00:26 +1100

    more added to js file, buttons work in testing, css amended for display, textarea added to html

--- assets/js/mainScript.js
+++ assets/js/mainScript.js
@@ -5,12 +5,101 @@ scheduler
 javascript file
 */
 
+function open(){
+    checkEvents();
+    elapse();
+}
 
 function elapse(){
     setInterval(timeInterval, 1000);
-
     function timeInterval(){
         document.getElementById("currentDay").innerHTML = moment().format("dddd, Do MMMM YYYY, h:mm:ss a");
-    }
+        var saved = localStorage.getItem("eventList");
+        var loop = 0;
+        var loopTime = 9;
 
+        while(loopTime < 13){
+
+            var check = saved[loop].event;
+            var name = loopTime + "text";
+            console.log(name);
+
+
+            loop++;
+            loopTime++;
+        }
+    }
+}
+
+function checkEvents(){
+    var eventList = localStorage.getItem("eventList");
+    /* if eventList is empty, set variable */
+    if(!eventList){
+        var block9={
+            time:"9am",
+            event:""
+        }
+        var block10={
+            time:"10am",
+            event:""
+        }
+        var block11={
+            time:"11am",
+            event:""
+        }
+        var block12={
+            time:"12pm",
+            event:""
+        }
+        var block13={
+            time:"1pm",
+            event:""
+        }
+        var block14={
+            time:"2pm",
+            event:""
+        }
+        var block15={
+            time:"3pm",
+            event:""
+        }
+        var block16={
+            time:"4pm",
+            event:""
+        }
+        var block17={
+            time:"5pm",
+            event:""
+        }
+        var block18={
+            time:"6pm",
+            event:""
+        }
+
+
+        localStorage.setItem("eventList","");
+    }
+}
+
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+var button3 = document.getElementById("btn3");
+var button4 = document.getElementById("btn4");
+
+button1.addEventListener("click",test1);
+button2.addEventListener("click",test2);
+button3.addEventListener("click",test3);
+button4.addEventListener("click",test4);
+
+function test1(){
+    button1.innerHTML = "test";
+}
+function test2(){
+    button2.innerHTML = "test";
+}
+function test3(){
+    button3.innerHTML = "test";
+}
+function test4(){
+    button4.innerHTML = "test";
 }
